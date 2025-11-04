a4e8bc8fd9e1057cc80878cf25de07c6aff232c7
Wilson-deve <nshimiyimanawilson91@gmail.com>
2025-11-04 18:27:04 +0200

    add products page and cart ui

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,20 @@
-const Homepage = () => {
-  return <div className="bg-red-300">Homepage</div>;
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
+const Homepage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) => {
+  const category = (await searchParams).category || "";
+  return (
+    <div className="">
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Featured" fill />
+      </div>
+      <ProductList category={category} params="homepage" />
+    </div>
+  );
 };
 
 export default Homepage;
